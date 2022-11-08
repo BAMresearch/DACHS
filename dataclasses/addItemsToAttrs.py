8c1914b65339a1793a99a06975dd97388ca11f6e
Brian Richard Pauw <brian.pauw@bam.de>
2022-11-08 09:50:05 +0100

    reagent now has a convenient items() iterable

---
+++ dataclasses/addItemsToAttrs.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Overview:
+========
+An add=on that will add an items() method to an attrs-created dataclass.
+Useful for iterating over keys, for example for storing in an HDF5 file.
+"""
+
+__author__ = "Brian R. Pauw"
+__contact__ = "brian@stack.nl"
+__license__ = "GPLv3+"
+__date__ = "2022/11/07"
+__status__ = "beta"
+
+from attrs import fields
+from typing import Any, NoReturn
+from collections.abc import Iterable
+
+# Mixin class for making a dict-like object out of an attrs class
+# from: https://github.com/python-attrs/attrs/issues/879
+
+
+class addItemsToAttrs:  # used to be MutableMappingMixin(MutableMapping)
+    """Mixin class to make attrs classes quack like a dictionary (well,
+    technically a mutable mapping). ONLY use this with attrs classes.
+
+    Provides keys(), values(), and items() methods in order to be
+    dict-like in addition to MutableMapping-like. Also provides pop(),
+    but it just raises a TypeError :)
+    """
+
+    __slots__ = ()  # May as well save on memory?
+
+    def __iter__(self) -> Iterable:
+        for ifield in fields(self.__class__):
+            yield ifield.name
+
+    def __len__(self) -> int:
+        return len(fields(self.__class__))
+
+    def __getitem__(self, k: str) -> Any:
+        """
+        Adapted from:
+        https://github.com/python-attrs/attrs/issues/487#issuecomment-660727537
+        """
+        try:
+            return self.__getattribute__(k)
+        except AttributeError as exc:
+            raise KeyError(str(exc)) from None
+
+    def __delitem__(self, v: str) -> NoReturn:
+        raise TypeError("Cannot delete fields for attrs classes.")
+
+    def __setitem__(self, k: str, v: Any) -> None:
+        self.__setattr__(k, v)
+
+    def pop(self, key, default=None) -> NoReturn:
+        raise TypeError("Cannot pop fields from attrs classes.")
+
+    def keys(self) -> Iterable:
+        return self.__iter__()
+
+    def values(self) -> Iterable:
+        for key in self.__iter__():
+            yield self.__getattribute__(key)
+
+    def items(self) -> Iterable:
+        for key in self.__iter__():
+            yield key, self.__getattribute__(key)
--- dataclasses/reagent.py
+++ dataclasses/reagent.py
@@ -4,7 +4,7 @@
 """
 Overview:
 ========
-A dataclass for specifying a reagent. 
+A dataclass for specifying a reagent.
 """
 
 __author__ = "Brian R. Pauw"
@@ -13,13 +13,15 @@ __license__ = "GPLv3+"
 __date__ = "2022/11/07"
 __status__ = "beta"
 
-from attrs import define, validators, field, cmp_using, fields
-from typing import List, Optional, Any, NoReturn
-from collections.abc import Iterable
+from attrs import define, validators, field
+from typing import Optional
+from addItemsToAttrs import addItemsToAttrs
 
 # TODO: we probably need to add a units class like Pint
+
+
 @define
-class reagent:
+class reagent(addItemsToAttrs):
     Name: str = field(
         default=None,
         validator=validators.instance_of(str),
@@ -80,3 +82,22 @@ class reagent:
         validator=validators.optional(validators.instance_of(float)),
         converter=float,
     )
+
+
+if __name__ == "__main__":
+    """Just a basic test of the class"""
+    solvent = reagent(
+        Name="Methanol",
+        ChemicalFormula="MeOH",
+        MolarMass=12.4,
+        Density=0.8,
+        CASNumber="1293847-2839147",
+        Brand="Absolut",
+        UNNumber="weori-198273",
+        MinimumPurity=0.98,
+        OpenDate="2022-05-01T10:04:22",
+        StorageConditions=None,
+        UnitPrice=12.4,
+        UnitSize=1,
+    )
+    print([f"{k}: {v}" for k, v in solvent.items()])
