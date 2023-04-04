029c20c94823a30c92b20b468b1db5fd78cb8ebd
Ingo Breßler <dev@ingobressler.net>
2023-04-04 18:04:48 +0200

    style(serialization): fix file header
    - previous one indicates an executable python file, resulting in automatic file mode change (exec) by the editor

--- src/dachs/serialization.py
+++ src/dachs/serialization.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
+# -*- coding: utf-8 -*-
+# serialization.py
 
 """
 Utility function to serialize data and meta classes for export, to a HDF5 file, for example.
