395b2cae8044b6091d49b7177c44d624cb3d00e3
Ingo Breßler <dev@ingobressler.net>
2023-06-06 20:23:34 +0200

    test(structure): using code from main to generate .h5 files, more dependencies

--- tests/test_structure.py
+++ tests/test_structure.py
@@ -1,55 +1,30 @@
 # -*- coding: utf-8 -*-
 # test_structure.py
 
+from os import environ
 from pathlib import Path
 
-from mcsas3 import McHDF
-
-import dachs.serialization
-import dachs.structure
+from dachs.main import main
 
 # import sys
 # defaultPath = Path(__file__).resolve().parent / "src"
 # sys.path.append(defaultPath)
 
+basepath = Path(__file__).parent / "testData"
+
 
 def test_integral() -> None:
-    basepath = Path("tests", "testData")
-    exp = dachs.structure.create(
-        basepath / "AutoMOFs_Logbook_Testing.xlsx",
-        [
-            basepath / "AutoMOFs05_Solution0.xlsx",
-            basepath / "AutoMOFs05_Solution1.xlsx",
-        ],
-        basepath / "AutoMOFs05_H005.xlsx",
-        amset="AMSET_6",
-    )
-
-    dump = dachs.serialization.dumpKV(exp)
-    storeItems = {k: v for k, v in dachs.serialization.filterStoragePaths(dump.items())}
-    # from pprint import pprint
-    # pprint(dump)
-    McHDF.storeKVPairs(f"{exp.ID}_H005.h5", "", storeItems.items())
-
-
-def test_integral_A6() -> None:
-    basepath = Path("tests", "testData")
-    exp = dachs.structure.create(
-        basepath / "AutoMOFs_Logbook_Testing.xlsx",
-        [
-            basepath / "log_AutoMOFs_6_Solution0.xlsx",
-            basepath / "log_AutoMOFs_6_Solution1.xlsx",
-        ],
-        basepath / "log_AutoMOFs_6_L019.xlsx",
-        amset="AMSET_6",
-    )
-
-    dump = dachs.serialization.dumpKV(exp)
-    storeItems = {k: v for k, v in dachs.serialization.filterStoragePaths(dump.items())}
-    # from pprint import pprint
-    # pprint(dump)
-    McHDF.storeKVPairs(f"{exp.ID}_L019.h5", "", storeItems.items())
+    environ["DACHS_LOGBOOK"] = str(basepath / "AutoMOFs_Logbook_Testing.xlsx")
+    environ["DACHS_AMSET"] = "AMSET_6"
+    for s0, s1, syn in (
+        ("AutoMOFs05_Solution0.xlsx", "AutoMOFs05_Solution1.xlsx", "AutoMOFs05_H005.xlsx"),
+        ("log_AutoMOFs_6_Solution0.xlsx", "log_AutoMOFs_6_Solution1.xlsx", "log_AutoMOFs_6_L019.xlsx"),
+    ):
+        environ["DACHS_SOL0"] = str(basepath / s0)
+        environ["DACHS_SOL1"] = str(basepath / s1)
+        environ["DACHS_SYNLOG"] = str(basepath / syn)
+        main()
 
 
 if __name__ == "__main__":
-    test_integral_A6()
+    test_integral()
